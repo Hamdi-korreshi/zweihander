deea0ae8c24e1c864ec203490d59b7f04317cf90
Hamdi-korreshi <hamdikorreshi256@gmail.com>
2025-09-24 07:55:02 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Small little website using Vue and maybe a Django backend for adding the blog posts and such much easier.
+
+Currently under progress.
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <p>Bruh its working</p>
+    <button class="btn">Bruh</button>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
