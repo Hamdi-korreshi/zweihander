0b7255981cda4b26308501f502fe01d15d853129
Hamdi-korreshi <hamdikorreshi@gmail.com>
2025-10-09 15:59:07 -0400

    added more projects and adding articles

---
+++ src/data/articles.js
@@ -0,0 +1,11 @@
+export const articles = [
+    {
+        id: 'wsl-walk',
+        slug: 'wsl-walk',
+        title: 'WSL2 + Docker',
+        excerpt: 'Walkthrough of how to install WSL2 and use it with Docker',
+        image: '/images/village.png',
+        tags: ['Web App', 'Full Stack'],
+        tech: ['Vue', 'Django', 'PostgreSQL', 'Docker'],
+    }
+]
--- src/data/projects.js
+++ src/data/projects.js
@@ -14,9 +14,49 @@ export const projects = [
         slug: 'cuda-bfs',
         title: 'GPU Bi-BFS',
         exerpt: 'CUPA + MPI bi-directional BFS implementation with perf notes',
-        image: '/public/images/cuda-bfs.png',
+        image: '/images/cuda-bfs.png',
         github: 'https://github.com/Tomaszbrauntsch/Bidirectional-BFS',
         tags: ['GPU', 'Algorithms'],
         tech: ['CUDA', 'MPI', 'C++']
+    },
+    {
+        id: 'compiler',
+        slug: 'cool-compiler',
+        title: 'COOL Compiler',
+        exerpt: 'The COOL (Classroom Object Orientanted Language) Compiler is a compiler for a Java-like language that can compile to x86-64',
+        image: '/images/compiler.png',
+        github: 'https://github.com/Hamdi-korreshi/CS488_compilers',
+        tags: ['Low-level', 'x86-64'],
+        tech: ['Ocaml', 'Bash', ]
+    },
+    {
+        id: 'youtube-algo',
+        slug: "youtube-algo",
+        title: "YouTube-like Algorithm",
+        exerpt: 'A showing of how to use a NN and encoding model to create a YouTube similarity aglorithm.',
+        image: '/images/similar.png',
+        github: '',
+        tags: [ 'AI', ],
+        tech: ['Pytorch', 'PostgreSQL', 'Docker']
+    },
+    {
+        id: 'geo-split',
+        slug: 'geo-split',
+        title: 'Geo Splitting Model',
+        exerpt: 'A model that uses the Geo spatial splitting model from Facebok and trained it to detect roads.',
+        image: '/images/example.png',
+        github: 'https://github.com/saumyad03/segment-anything-project',
+        tags: ['AI', 'Patchify'],
+        tech: ['PyTorch', 'Google Colab', 'Flask']
+    },
+    {
+        id: 'codecraft',
+        slug: 'codecraft',
+        title: 'CodeCraft',
+        exerpt: '',
+        image: '/images/codecraft.png',
+        tags: ['Web App', 'Full Stack'],
+        github: '',
+        tech: ['React', 'Flask', 'PostgreSQL']
     }
 ]
